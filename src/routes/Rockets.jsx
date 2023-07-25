0337b56f012d9f03bc60ecef9ebf31d40cc9630e
Fombi-Favour <fombifavour@gmail.com>
2023-07-25 14:53:32 +0100

    Update Rockets.jsx

--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rockets = () => (
   <div className="flex justify-center">
-    <div className="flex max-w-wrap w-full p-1">Rockets</div>
+    <div className="flex max-w-wrap w-full p-1">Rockets section</div>
   </div>
 );
 
