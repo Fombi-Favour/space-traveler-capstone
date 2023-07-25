deccc91ad50b21dd8bb62fbddee4553085a78a2d
Fombi-Favour <fombifavour@gmail.com>
2023-07-25 14:52:55 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './missions/missionsSlice';
+import RocketReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
     mission: missionReducer,
+    rocket: RocketReducer,
   },
 });
 
